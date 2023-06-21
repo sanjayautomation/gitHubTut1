f2ed38341c59307311c42e5c38ce89f97228b030
sanjayautomation <treestechnospace@gmail.com>
2023-06-21 17:27:37 +0530

    next time

---
+++ src/main/java/PageLayer/PIMPage.java
@@ -0,0 +1,11 @@
+package PageLayer;
+
+public class PIMPage {
+
+	public static void main(String[] args) {
+		System.out.println("PIMPage class");
+		
+
+	}
+
+}
---
+++ src/test/java/TestLayer/PIMPageTest.java
@@ -0,0 +1,10 @@
+package TestLayer;
+
+public class PIMPageTest {
+
+	public static void main(String[] args) {
+		System.out.println("PIMPage Test class");
+
+	}
+
+}
