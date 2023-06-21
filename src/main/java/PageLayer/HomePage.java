c0f3e8e5d9efb32a3ced94332328c1c91537eab9
sanjayautomation <treestechnospace@gmail.com>
2023-06-21 17:18:35 +0530

    anjali code

---
+++ src/main/java/PageLayer/HomePage.java
@@ -0,0 +1,11 @@
+package PageLayer;
+
+public class HomePage {
+
+	public static void main(String[] args) {
+		System.out.println("HomePage class");
+		System.out.println("HomePage class2");
+
+	}
+
+}
---
+++ src/test/java/TestLayer/HomePageTest.java
@@ -0,0 +1,11 @@
+package TestLayer;
+
+public class HomePageTest {
+
+	public static void main(String[] args) {
+		System.out.println("HomePageTest");
+		System.out.println("HomePageTest class");
+
+	}
+
+}
